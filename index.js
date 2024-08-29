0d833152ea886b58f5c89ed2fc4590ca4b48628d
Cibele Caroline de Oliveira Guidek <cibele.guidek@escola.pr.gov.br>
2024-08-29 01:27:14 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let anoatual = 2024;
+const nomeCompleto = "Zibele";
+let idade = 16;
+const cursoFaculdade = "Engenharia da computação";
+let cidadeatual = "Mondstad";
+console.log(`ola eu sou a ${nomeCompleto}, eu tenho ${idade} de idade, moro em ${cidadeatual}, e ano que vem quero fazer ${cursoFaculdade}`);
+
+
+anoatual = 2025;
+idade = 17;
+
+
+console.log(`Para meu ano que vem, desejo já ter um emprego e começar a juntar dinheiro para a faculdade. Agora tenho ${idade}, e meu nome agora mudou para Cibele, não mais sendo ${nomeCompleto} E eu moro em sao jose dos pinhais agora, não mais em ${cidadeatual}`);
