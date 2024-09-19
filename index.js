44808e75b33d5d99f5e76a2d0c7331e50e416a77
Cibele Caroline de Oliveira Guidek <cibele.guidek@escola.pr.gov.br>
2024-09-19 01:38:09 +0000

    18/9/24

--- index.js
+++ index.js
@@ -11,3 +11,23 @@ idade = 17;
 
 
 console.log(`Para meu ano que vem, desejo já ter um emprego e começar a juntar dinheiro para a faculdade. Agora tenho ${idade}, e meu nome agora mudou para Cibele, não mais sendo ${nomeCompleto} E eu moro em sao jose dos pinhais agora, não mais em ${cidadeatual}`);
+
+//Agencia de vaiagens
+
+const nomeComprador = "Kaeya";
+const cpfComprador = 19875902827222;
+const idadeComprador = 20;
+const listadeDestinos = new Array(
+"bardoseujudas",
+"boteco",
+"CasadoDilucvulgobebidagratis",
+"Casadaklee",
+"França"
+);
+
+console.log(`Olá senhor ${nomeComprador}!, seu cpf e idade atual consta como o seguinte: ${cpfComprador}, e ${idadeComprador}. Suas compras em nosso sistema já foram devidamente confirmadas, e suas viagens foram marcadas para os seguintes destinos: ${listadeDestinos}`);
+
+console.log(listadeDestinos[0]);
+console.log(`Olá senhor ${nomeComprador}!, seu novo destino já foi adicionado a sua lista de interesses!`);
+listadeDestinos.push("Quartel-general dos cavaleiros de favonius");
+console.log(listadeDestinos);
